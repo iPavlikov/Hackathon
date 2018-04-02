6bc4471a9ba86e0843e855800805e07c1fdca710
Иван Павликов <ivanp220598@gmail.com>
2018-04-02 23:55:32 +0300

    Changed border on hover

--- app/js/script.js
+++ app/js/script.js
@@ -19,31 +19,23 @@ jQuery(document).ready(function($) {
 
   	// Add scrollspy to <body>
 	$('body').scrollspy({target: "#navbar", offset: 0});
-
 	// Add smooth scrolling on all links inside the navbar
 	$("#navbar a").on('click', function(event) {
-
 	  // Make sure this.hash has a value before overriding default behavior
 	  if (this.hash !== "") {
-
 	    // Prevent default anchor click behavior
 	    event.preventDefault();
-
 	    // Store hash
 	    var hash = this.hash;
-
 	    // Using jQuery's animate() method to add smooth page scroll
 	    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
 	    $('html, body').animate({
 	      scrollTop: $(hash).offset().top
 	    }, 600, function(){
-
 	    // Add hash (#) to URL when done scrolling (default click behavior)
 	      window.location.hash = hash;
 	    });
-
 	  } // End if
-
 	});
 
 	$('.navbar-brand').on("click",function() {
@@ -58,5 +50,4 @@ jQuery(document).ready(function($) {
 	$('.navbar-brand').click(function(){
     	$(".navbar-collapse").collapse('hide');
 	});
-
 });
